e183117a117a03713fc967117f6fe60742d55849
dpyzo0o <yaxu@student.ethz.ch>
2018-05-15 16:36:00 +0200

    add roman-to-integer

---
+++ algorithms/013-roman-to-integer.js
@@ -0,0 +1,72 @@
+/**
+ *
+ * Source: https://leetcode.com/problems/roman-to-integer/
+ * Date  : 2018-05-15
+ *
+ * Roman numerals are represented by seven different symbols:
+ * I, V, X, L, C, D and M.
+ *
+ * Symbol  Value
+ * I       1
+ * V       5
+ * X       10
+ * L       50
+ * C       100
+ * D       500
+ * M       1000
+ *
+ * For example, two is written as II in Roman numeral, just two one's added
+ * together. Twelve is written as, XII, which is simply X + II. The number
+ * twenty seven is written as XXVII, which is XX + V + II.
+ *
+ * Roman numerals are usually written largest to smallest from left to right.
+ * However, the numeral for four is not IIII. Instead, the number four is
+ * written as IV. Because the one is before the five we subtract it making
+ * four. The same principle applies to the number nine, which is written as
+ * IX. There are six instances where subtraction is used:
+ *
+ * - I can be placed before V (5) and X (10) to make 4 and 9.
+ * - X can be placed before L (50) and C (100) to make 40 and 90.
+ * - C can be placed before D (500) and M (1000) to make 400 and 900.
+ *
+ * Given a roman numeral, convert it to an integer. Input is guaranteed to be
+ * within the range from 1 to 3999.
+ *
+ * Idea:
+ *
+ * Look at the six instances where subtraction is used, the value of front
+ * letter is smaller. Thus, loop through the letters, subtract the previous one
+ * if smaller, otherwise add the value.
+ *
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+  var result = 0;
+
+  for (var i = 0; i < s.length - 1; i++) {
+    var current = toNum[s[i]];
+    var next = toNum[s[i + 1]];
+
+    if (current < next) {
+      result -= current;
+    } else {
+      result += current;
+    }
+  }
+
+  return result + toNum[s[s.length - 1]];
+};
+
+var toNum = {
+  M: 1000,
+  D: 500,
+  C: 100,
+  L: 50,
+  X: 10,
+  V: 5,
+  I: 1
+};
