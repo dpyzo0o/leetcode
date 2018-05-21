a3bf311a77f185ca3f29662dcb92392ddccbaed2
dpyzo0o <yaxu@student.ethz.ch>
2018-05-21 15:45:14 +0200

    add length-of-last-word

---
+++ algorithms/058-length-of-last-word.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * Source: https://leetcode.com/problems/length-of-last-word/
+ * Date  : 2018-05-21
+ *
+ * Given a string s consists of upper/lower-case alphabets and empty space
+ * characters ' ', return the length of last word in the string. If the last
+ * word does not exist, return 0.
+ *
+ *
+ * Example:
+ *
+ * Input: "Hello World"
+ * Output: 5
+ *
+ * Idea:
+ *
+ * Easy problem, just be careful with some special cases. "" and "a " should
+ * return 0 and 1.
+ *
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+  s = s.trim().split(" ");
+  return s[s.length - 1].length;
+};
