ddd9dc8b9c258bb99beecdfcf276cc689edbd5e7
yang <yang.xu940121@outlook.com>
2018-08-04 15:23:13 +0200

    add 189

---
+++ algorithms/189-rotate-array.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * Source: https://leetcode.com/problems/rotate-array/
+ * Date  : 2018-08-04
+ *
+ * Given an array, rotate the array to the right by k steps, where k is non-negative.
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ *
+ * Example 2:
+ *
+ * Input: [-1,-100,3,99] and k = 2
+ * Output: [3,99,-1,-100]
+ *
+ * Idea:
+ *
+ * Straightforward, code explains
+ *
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+  nums.unshift(...nums.splice(nums.length - k, k));
+};
