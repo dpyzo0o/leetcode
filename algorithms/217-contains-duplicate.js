659db5b8c6d52966a4c6d87f47e1768fea9833e8
yang <yang.xu940121@outlook.com>
2018-08-08 17:48:19 +0200

    add 217

---
+++ algorithms/217-contains-duplicate.js
@@ -0,0 +1,43 @@
+/**
+ *
+ * Source: https://leetcode.com/problems/contains-duplicate/
+ * Date  : 2018-08-08
+ *
+ * Given an array of integers, find if the array contains any duplicates.
+ * Your function should return true if any value appears at least twice in the array,
+ * and it should return false if every element is distinct.
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3,1]
+ * Output: true
+ *
+ * Example 2:
+ *
+ * Input: [1,2,3,4]
+ * Output: false
+ *
+ * Idea:
+ *
+ * Use ES6 set structure
+ *
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  if (nums.length < 2) return false;
+
+  let set = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (!set.has(nums[i])) {
+      set.add(nums[i]);
+    } else {
+      return true;
+    }
+  }
+
+  return false;
+};
