56ca3f0838fcb7b1192a2fc8e6466da4be8f1f02
dpyzo0o <yaxu@student.ethz.ch>
2018-08-09 13:48:22 +0200

    add 231

---
+++ algorithms/231-power-of-two.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * Source: https://leetcode.com/problems/power-of-two/
+ * Date  : 2018-08-09
+ *
+ * Given an integer, write a function to determine if it is a power of two.
+ *
+ * Idea:
+ *
+ * If n is a power of 2, then n & n - 1 must be 0.
+ *
+ */
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+  return n > 0 && !(n & (n - 1));
+};
