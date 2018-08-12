d2dc22e1bbe3e1f38ca6e5acd8037961b9c640bc
dpyzo0o <yaxu@student.ethz.ch>
2018-08-12 16:48:37 +0200

    add 257

---
+++ algorithms/257-binary-tree-paths.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * Source: https://leetcode.com/problems/binary-tree-paths/
+ * Date  : 2018-08-12
+ *
+ * Given a binary tree, return all root-to-leaf paths.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ *
+ * Input:
+ *     1
+ *   /   \
+ *  2     3
+ *   \
+ *    5
+ *
+ * Output: ['1->2->5', '1->3']
+ *
+ * Idea:
+ *
+ * DFS
+ *
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *   this.val = val;
+ *   this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+  let res = [];
+  if (root !== null) dfs(root, '', res);
+  return res;
+};
+
+const dfs = (node, path, arr) => {
+  if (node.left === null && node.right === null) arr.push(path + node.val);
+  if (node.left !== null) dfs(node.left, path + node.val + '->', arr);
+  if (node.right !== null) dfs(node.right, path + node.val + '->', arr);
+};
