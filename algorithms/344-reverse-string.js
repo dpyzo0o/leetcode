49343cefcfd203c567c525a297a3f0455c271f49
dpyzo0o <yaxu@student.ethz.ch>
2018-08-20 19:39:14 +0200

    add 344

---
+++ algorithms/344-reverse-string.js
@@ -0,0 +1,36 @@
+/**
+ *
+ * Source: https://leetcode.com/problems/reverse-string/
+ * Date  : 2018-08-20
+ *
+ * Write a function that takes a string as input and returns the string reversed.
+ *
+ * Example 1:
+ *
+ * Input: 'hello'
+ * Output: 'olleh'
+ *
+ * Example 2:
+ *
+ * Input: 'A man, a plan, a canal: Panama'
+ * Output: 'amanaP :lanac a ,nalp a ,nam A'
+ *
+ * Idea:
+ *
+ * Code explains.
+ *
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseString = function(s) {
+  let res = '';
+
+  for (let i = 0; i < s.length; i++) {
+    res = s[i] + res;
+  }
+
+  return res;
+};
